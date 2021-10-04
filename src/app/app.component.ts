9f76e21e1554f1aa5dd84ad08c4f15f2aa6e57ea
John Enrick Gornez <john.gornez.20@usjr.edu.ph>
2021-10-04 14:14:26 +0800

    Added Angular calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-calculator';
+  num1!:number;
+  num2!:number;
+  result!:number;
+
+  getValue(value:any){
+    this.num1=parseInt(value);
+  }
+
+  getValue2(value:any){
+    this.num2=parseInt(value);
+  }
+
+  add(){
+    this.result=this.num1+this.num2;
+  }
+
+  sub(){
+    this.result=this.num1-this.num2;
+  }
+
+  mul(){
+    this.result=this.num1*this.num2;
+  }
+
+  div(){
+    this.result=this.num1/this.num2;
+  }
 }
